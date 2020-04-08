2e5a4f0cbbe81180be5ec25a3f8cb433758015eb
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-04-08 11:48:51 -0300

    Create core.py

---
+++ core.py
@@ -0,0 +1,90 @@
+# package name = fuzzc
+class Crisp_Set():
+  def __init__(self, crisp_set, membership_fn):
+    # initialize
+    self.crisp_set = crisp_set
+    self.membership_fn = membership_fn
+  
+  def show(self):
+    out = []
+    assert len(self.crisp_set) == len(self.membership_fn)
+    
+    if len(self.crisp_set) >= 1:
+      for item, fn in zip(crisp_set, membership_fn):
+        out.append(tuple((item, fn)))
+    print(out)
+
+class Fuzzy_Set():
+  def __init__(self, fuzzy_set, membership_fn):
+    # initialize
+    self.fuzzy_set = fuzzy_set
+    self.membership_fn = membership_fn
+  
+  def __str__(self):
+    return f'Fuzzy_Control Fuzzy_Set instance with a length of {len(self.fuzzy_set)}'
+
+  def __add__(self, y):
+    try:
+      assert len(self.membership_fn) == len(y.membership_fn)
+      fn = self.membership_fn+y.membership_fn
+      fn[fn >=1] = 1
+      return (self.fuzzy_set, fn)
+    except:
+      print('Wrong input!')
+      return None
+
+  def show(self):
+    out = []
+    assert len(self.fuzzy_set) == len(self.membership_fn)   
+    if len(self.fuzzy_set) >= 1:
+      for item, fn in zip(fuzzy_set, membership_fn):
+        out.append(tuple((item, fn)))
+    print(out)
+    del out
+    return None
+  
+  def support(self):
+    fuzzy_set = self.fuzzy_set[self.membership_fn >= 0.0]
+    membership_fn = self.membership_fn[self.membership_fn >= 0.0]
+    return fuzzy_set, membership_fn
+
+  def crossover(self):
+    fuzzy_set = self.fuzzy_set[self.membership_fn == 0.5]
+    membership_fn = self.membership_fn[self.membership_fn == 0.5]
+    return fuzzy_set, membership_fn
+
+  def is_fuzzy_singleton(self):
+    if len(self.fuzzy_set[self.membership_fn >= 0.0]) == 1:
+      return True
+    else:
+      return False
+        
+  def center(self):
+    # rewrite for a bigger center
+    fuzzy_set = self.fuzzy_set[self.membership_fn >= np.amax(self.membership_fn)]
+    membership_fn = self.membership_fn[self.membership_fn >= np.amax(self.membership_fn)]
+    return fuzzy_set, membership_fn
+
+  def plot_membership(self):
+    try:
+      import matplotlib.pyplot as plt
+      plt.plot(self.fuzzy_set, self.membership_fn, 'b')
+    except:
+      print('Could not import matplotlib. Make sure to install.')
+
+  def height(self):
+    return np.amax(self.membership_fn)
+
+  def alpha_cut(self, alpha=0.5):
+    fuzzy_set = self.fuzzy_set[self.membership_fn >= alpha]
+    membership_fn = np.ones(fuzzy_set.shape)
+    return fuzzy_set, membership_fn
+
+  def core(self):
+    fuzzy_set = self.fuzzy_set[self.membership_fn == 1]
+    membership_fn = self.membership_fn[self.membership_fn == 1]
+    return fuzzy_set, membership_fn
+
+  def is_convex(self):
+    # Incomplete
+    return True
