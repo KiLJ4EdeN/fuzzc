99ff853585f984aac53fd99dda55b0eafc8ebb8e
Abdolkarim Saeedi <parsa592323@gmail.com>
2020-07-02 17:26:18 +0000

    Update core.py

--- fuzzc/core.py
+++ fuzzc/core.py
@@ -1,18 +1,13 @@
 # package name = fuzzc
-# python setup.py sdist
-# v_01 twine upload dist/*
-import numpy as np
-import matplotlib.pyplot as plt
-
-
 class Fuzzy_Set():
   # built ins
   def __init__(self, fuzzy_set, membership_fn):
-    # initialize
+    
     self.fuzzy_set = fuzzy_set
     self.membership_fn = membership_fn
   
   def __str__(self):
+    # for when the object is printed out.
     return f'Fuzzy_Control Fuzzy_Set instance with a length of {len(self.fuzzy_set)}'
 
   def __add__(self, y):
@@ -98,6 +93,11 @@ class Fuzzy_Set():
       return True
   # class methods.
   def show(self):
+    """
+    Show a fuzzy set and memberships is tuple pairs.
+    :param self: a Fuzzy_Set instance.
+    :return: a list containing the pairs.
+    """
     out = []
     assert len(self.fuzzy_set) == len(self.membership_fn)   
     if len(self.fuzzy_set) >= 1:
@@ -108,6 +108,11 @@ class Fuzzy_Set():
     return None
   
   def support(self):
+    """
+    Compute where the fuzzy memberships are bigger than zero.
+    :param self: a Fuzzy_Set instance.
+    :return: a Fuzzy_Set with memberships greater than zero.
+    """
     fuzzy_set = self.fuzzy_set[self.membership_fn > 0.0]
     membership_fn = self.membership_fn[self.membership_fn > 0.0]
     return Fuzzy_Set(fuzzy_set, membership_fn)
@@ -118,18 +123,33 @@ class Fuzzy_Set():
     return Fuzzy_Set(fuzzy_set, membership_fn)
 
   def is_fuzzy_singleton(self):
+    """
+    A fuzzy set with a support of length one.
+    :param self: a Fuzzy_Set instance.
+    :return: True if the set is singleton else False.
+    """
     if len(self.fuzzy_set[self.membership_fn >= 0.0]) == 1:
       return True
     else:
       return False
         
   def center(self):
-    # rewrite for a bigger center
+    # TODO: rewrite for a bigger center
+    """
+    Compute the fuzzy set center.
+    :param self: a Fuzzy_Set instance.
+    :return: Fuzzy_Set with maximum membership.
+    """
     fuzzy_set = self.fuzzy_set[self.membership_fn >= np.amax(self.membership_fn)]
     membership_fn = self.membership_fn[self.membership_fn >= np.amax(self.membership_fn)]
     return Fuzzy_Set(fuzzy_set, membership_fn)
 
   def plot_membership(self):
+    """
+    Plot the fuzzy set membership.
+    :param self: a Fuzzy_Set instance.
+    :return: membership plot using matplotlib.
+    """
     try:
       import matplotlib.pyplot as plt
       plt.plot(self.fuzzy_set, self.membership_fn, 'b')
@@ -137,23 +157,55 @@ class Fuzzy_Set():
       print('Could not import matplotlib. Make sure to install.')
 
   def height(self):
+    """
+    Compute the fuzzy set height.
+    :param self: a Fuzzy_Set instance.
+    :return: Maximum membership.
+    """
     return np.amax(self.membership_fn)
 
   def alpha_cut(self, alpha=0.5):
+    """
+    Compute the fuzzy set alpha cut.
+    :param self: a Fuzzy_Set instance.
+    :param alpha: cut value for membership_fn.
+    :return: Fuzzy_Set with memberships bigger than selected alpha.
+    """
     fuzzy_set = self.fuzzy_set[self.membership_fn >= alpha]
     membership_fn = np.ones(fuzzy_set.shape)
     return Fuzzy_Set(fuzzy_set, membership_fn)
 
   def core(self):
+    """
+    Compute the fuzzy set core.
+    :param self: a Fuzzy_Set instance.
+    :return: Fuzzy_Set with membership equal to "1".
+    """
     fuzzy_set = self.fuzzy_set[self.membership_fn == 1]
     membership_fn = self.membership_fn[self.membership_fn == 1]
     return Fuzzy_Set(fuzzy_set, membership_fn)
 
   def is_normal(self):
+    """
+    Check if the fuzzy set is normal.
+    :param self: a Fuzzy_Set instance.
+    :return: True if maximum membership is "1" ow False.
+    """
     if np.amax(self.membership_fn) == 1:
       return True
     else:
       return False
+
+  def is_subnormnal(self):
+    """
+    Check if the fuzzy set is subnormal.
+    :param self: a Fuzzy_Set instance.
+    :return: False if maximum membership is "1" ow True.
+    """
+    if np.amax(self.membership_fn) == 1:
+      return False
+    else:
+      return True
 
   def is_convex(self):
     # Incomplete
